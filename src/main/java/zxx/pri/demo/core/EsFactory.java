3b361d3e0d015047310039db049428b9b35dfd6b
zhouxiaoxiao <1139835238@qq.com>
2019-10-15 09:54:06 +0800

    集群连接方式跟单机有差异

--- src/main/java/zxx/pri/demo/core/EsFactory.java
+++ src/main/java/zxx/pri/demo/core/EsFactory.java
@@ -5,6 +5,8 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,17 +20,21 @@ import java.net.UnknownHostException;
  */
 @Configuration
 public class EsFactory {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    //    private final String ip = "10.253.90.84";
+    private final String ip = "10.253.90.90";
+    private final String localIp = "127.0.0.1";
+
     @Bean(value = "esClient")
     public Client client() {
-        Settings settings = Settings.builder().put("cluster.name", "ClusterName")//集群名称
-                .put("client.transport,sniff", true) //设置客户端监控集群状态 自动把机器ip地址加到客户端
+        Settings settings = Settings.builder().put("cluster.name", "my-test")//集群名称
+                .put("client.transport.sniff", true) //设置客户端监控集群状态 自动把机器ip地址加到客户端
                 .build();
         TransportClient client = null;
         try {
-            client = new PreBuiltTransportClient(Settings.EMPTY)
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+            client = new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ip), 9300));
         } catch (UnknownHostException e) {
-            e.printStackTrace();
+            log.error("连接es服务器异常 {} ", e);
         }
         return client;
     }
