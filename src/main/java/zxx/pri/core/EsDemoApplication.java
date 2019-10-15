b5fa49239731d720faea3af6e9d0e9cb0e1947ad
riceincoke <42928623+riceincoke@users.noreply.github.com>
2019-10-15 20:54:21 +0800

    es 2种连接方式学习
    Signed-off-by: riceincoke <42928623+riceincoke@users.noreply.github.com>

--- src/main/java/zxx/pri/core/EsDemoApplication.java
+++ src/main/java/zxx/pri/core/EsDemoApplication.java
@@ -13,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class EsDemoApplication {
     public static void main(String[] args) {
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(EsDemoApplication.class, args);
     }
 }
